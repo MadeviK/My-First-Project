55068c01acf8d2608f7f3957c8d1852304232fe0
MadeviK <madevi362005@gmail.com>
2026-02-22 12:47:52 +0530

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,19 @@
+
+ document.getElementById("redBtn").onclick = function() {
+            window.location.href = "inside.html";
+        };
+        document.getElementById("yellowBtn").onclick = function() {
+                  window.location.href = "inside2.html";
+        };
+        document.getElementById("blackBtn").onclick = function() {
+                  window.location.href="inside3.html";
+        };
+        document.getElementById("greenBtn").onclick = function() {
+                  window.location.href="inside4.html";
+        };
+
+        // Go to Add Page function
+       
+      
+
+        
